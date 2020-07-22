192b0cea6a73e6255c32579131650fdaf2156ba8
RuchiS26 <rchi.singhal@gmail.com>
2020-07-22 10:53:30 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,102 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//SetUp handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather Updates',
+        name: 'Ruchi Singhal'
+    })
+})
+
+app.get('/about',(req, res) => {
+    res.render('about',{
+        title: 'About Us!',
+        name: 'Ruchi Singhal'
+    })
+})
+
+app.get('/help',(req, res) => {
+    res.render('help', {
+        helpText: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting',
+        title: 'Help',
+        name: 'Ruchi Singhal'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'No address, Please Provide!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, location,(error, forecastData) => {
+            console.log(latitude, longitude)
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send ({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term!'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send ({
+        products: []
+    })
+})
+
+app.get('/help/*',(req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Alice',
+        errorMessage: 'Help Article Not Found!'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Alice',
+        errorMessage: 'Page Not Found!'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
